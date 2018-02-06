4f647ef37afaaa0a678b1df52d56a586797777d6
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-02-06 22:56:50 +0100

    Revert server settings

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const express = require('express');
 const logger = require('./logger');
 
 /* eslint-disable import/no-extraneous-dependencies */
+const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
 /* eslint-enable import/no-extraneous-dependencies */
@@ -19,13 +22,13 @@ setup(app, {
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = process.env.HOST;
+const customHost = argv.host || process.env.HOST;
 
 // Let http.Server use its default IPv6/4 host
 const host = customHost || null;
 const prettyHost = customHost || 'localhost';
 
-const port = process.env.PORT || 3000;
+const port = argv.port || process.env.PORT || 3000;
 
 // Start your app.
 app.listen(port, host, (err) => {
@@ -33,5 +36,16 @@ app.listen(port, host, (err) => {
     return logger.error(err.message);
   }
 
-  logger.appStarted(port, prettyHost);
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    ngrok.connect(port, (innerErr, url) => {
+      if (innerErr) {
+        return logger.error(innerErr);
+      }
+
+      logger.appStarted(port, prettyHost, url);
+    });
+  } else {
+    logger.appStarted(port, prettyHost);
+  }
 });
